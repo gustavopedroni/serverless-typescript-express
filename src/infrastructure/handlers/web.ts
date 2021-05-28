5f2babed43b2e0af030e744a5df7474f1e1c9e4e
Gustavo Pedroni <gustavo.pedroni@ezlogic.com>
2021-05-28 14:55:03 -0300

    initial commit

--- src/infrastructure/handlers/web.ts
+++ src/infrastructure/handlers/web.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-param-reassign */
-
 import { APIGatewayProxyEvent, Context } from 'aws-lambda'
 import serverless from 'serverless-http'
 
@@ -9,7 +5,13 @@ import app from '@src/infrastructure/web/app'
 
 const serverlessHandler = serverless(app)
 
-export async function handler(event: APIGatewayProxyEvent, context: Context) {
+export default async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<
+  AWSLambda.APIGatewayProxyResult | AWSLambda.APIGatewayProxyStructuredResultV2
+> {
+  // eslint-disable-next-line no-param-reassign
   event.path = event.path === '' ? '/' : event.path
 
   const result = await serverlessHandler(event, context)
